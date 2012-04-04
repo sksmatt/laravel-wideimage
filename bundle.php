a00a1ec711ec416551fc834c8a4d001146bda0a5
Matt <Matt@m.local>
2012-04-03 21:15:03 -0300

    - pushit

---
+++ bundle.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ Copy this to your application/bundles.php file
+*/
+
+return array(
+    'wideimage' => array(
+        'autoloads' => array(
+            'map' => array(
+                'WideImage' => '(:bundle)/lib/WideImage.php',
+            ),
+        ),
+    ),
+);
